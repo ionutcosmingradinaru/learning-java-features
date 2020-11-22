75b88e68b4c9607339fc0f851ca0b52d61b7b98b
Ionut Gradinaru <ionut.cosmin.gradinaru@gmail.com>
2020-11-22 18:22:42 +0200

    [Functional programming]: Using the Functions from Java and implement Combinator Pattern

--- functional_interfaces_feature/src/functional_interfaces/Main.java
+++ functional_interfaces_feature/src/functional_interfaces/Main.java
@@ -1,6 +1,9 @@
 package functional_interfaces;
 
 import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Main {
 
@@ -9,11 +12,26 @@ public class Main {
 
     var names = List.of("John", "Sofia", "Tatiana", "Anastasia");
     print(names, s -> s.startsWith("T"));
+
+    UnaryOperator<String> uppercaseByFunction = s -> s.toUpperCase();
+    var resultUpperFunction = uppercaseByFunction.apply("john");
+    System.out.println(resultUpperFunction);
+
+    BinaryOperator<String> concatFunction = (s1, s2) -> s1 + s2;
+    var resultConcatFunction = concatFunction.apply("John", "Tatiana");
+    System.out.println(resultConcatFunction);
+
+    var person = new Person("John","john@icloud.com");
+    var resultValidation = PersonValidator
+        .isNameValid()
+        .and(PersonValidator.isEmailValid())
+        .apply(person);
+    System.out.println(resultValidation);
   }
 
-  private static void print(MyPrintable moveable) {
-    moveable.print();
-    moveable.defaultMessage();
+  private static void print(MyPrintable movable) {
+    movable.print();
+    movable.defaultMessage();
   }
 
   private static void print(List<String> names, MyFilter<String> filter) {
@@ -39,4 +57,34 @@ interface MyPrintable {
 interface MyFilter<T> {
 
   boolean test(T obj);
+}
+
+// Combinator Patter
+record Person(String name, String email) {
+}
+
+enum ValidationResult {
+  SUCCESS,
+  NAME_NOT_VALID,
+  EMAIL_NOT_VALID
+}
+
+interface PersonValidator extends Function<Person, ValidationResult> {
+
+  static PersonValidator isNameValid() {
+    return p -> (p.name() != null && !p.name().strip().isEmpty())
+        ? ValidationResult.SUCCESS : ValidationResult.NAME_NOT_VALID;
+  }
+
+  static PersonValidator isEmailValid() {
+    return p -> (p.email() != null && p.email().contains("@"))
+        ? ValidationResult.SUCCESS : ValidationResult.EMAIL_NOT_VALID;
+  }
+
+  default PersonValidator and(PersonValidator other) {
+    return p -> {
+      var result = this.apply(p);
+      return result.equals(ValidationResult.SUCCESS) ? other.apply(p) : result;
+    };
+  }
 }
